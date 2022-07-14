a6b05f2e76fdb298ecd8154ea8607fc9912d6337
kmv-dev <kmvdev@yandex.ru>
2022-07-14 12:35:52 +0700

    add scss and update layout

---
+++ src/components/Header/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <div class="header">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background: black;
+}
+</style>
