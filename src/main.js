837a9127e4c73db9abc7e145dd1a9943e85d66e7
kmv-dev <kmvdev@yandex.ru>
2022-07-16 00:36:24 +0700

    update add vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
+import store from "./store";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(store);
 app.use(router);
 
 app.mount("#app");
